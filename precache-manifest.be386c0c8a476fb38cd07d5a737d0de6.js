fe5de7caed710469f695a5ab26ba1330f58ea73c
KLIJIN <mdu091008@yandex.ru>
2021-02-12 19:29:21 +0700

    Updates

---
+++ precache-manifest.be386c0c8a476fb38cd07d5a737d0de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f663e7ab30ab5a9165a7d2ec83f2057",
+    "url": "/redux-example/index.html"
+  },
+  {
+    "revision": "ca5360cbeece192bc836",
+    "url": "/redux-example/static/css/main.c463a086.chunk.css"
+  },
+  {
+    "revision": "f8a87a0215f7ca3ebdc2",
+    "url": "/redux-example/static/js/2.36edcc9b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/redux-example/static/js/2.36edcc9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5360cbeece192bc836",
+    "url": "/redux-example/static/js/main.c06bc80a.chunk.js"
+  },
+  {
+    "revision": "5a9d8173edd7354f797d",
+    "url": "/redux-example/static/js/runtime-main.7e0218db.js"
+  }
+]);
